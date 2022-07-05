05f101d3b2c7de0c67d3da3b31bb1b4dc28d908e
grzehoo <kazimierski.grzegorz@gmail.com>
2022-07-05 17:00:40 +0200

    update message on failed request

--- src/modules/contactsList/contactsList.settings.ts
+++ src/modules/contactsList/contactsList.settings.ts
@@ -1,5 +1,5 @@
 export const REQUEST_FAILED_MESSAGE =
-  "Whoops! Something went wrong, please check your internet connection.";
+  "Whoops! Something went wrong, please check your internet connection and try again.";
 export const TRY_AGAIN = "Retry";
 export const SELECTED_CONTACTS = "Selected contacts: ";
 export const LOAD_MORE = "Load more";
